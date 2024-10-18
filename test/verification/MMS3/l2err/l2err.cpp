5549cf79746c8cf2abf65b85bbf5f4f2ced97fc1
Hariswaran Sitaraman <hsitaram@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-10-17 22:26:59 -0600

    adding MMS3 to CI

--- test/verification/MMS3/l2err/l2err.cpp
+++ test/verification/MMS3/l2err/l2err.cpp
@@ -7,6 +7,7 @@
 #include <limits>
 #include <iterator>
 #include <fstream>
+#define PI 3.14159265358979323846264338327950288
 
 // compute the integral of f dV, where f is one of the fields in the plotfile
 // this understands axisymmetric geometry.
@@ -18,25 +19,11 @@ void main_main()
     const int narg = amrex::command_argument_count();
 
     std::string pltfile;
-    int dir;
+    int dir=0;
     amrex::Real n0=1e6;
-    amrex::Real alpha=1.60217662e-19/8.854187817e-12;
+    amrex::Real fintime=0.1;
 
     int farg = 1;
-    while (farg <= narg) 
-    {
-        const std::string& name = amrex::get_command_argument(farg);
-        if (name == "-dir") 
-        {
-            dir = std::stoi(amrex::get_command_argument(++farg));
-        } 
-        else 
-        {
-            break;
-        }
-        ++farg;
-    }
-
     if (pltfile.empty() && farg <= narg) 
     {
         pltfile = amrex::get_command_argument(farg);
@@ -52,8 +39,6 @@ void main_main()
     Vector<Real> pos;
     Real errorsum_e = 0.0;
     Real electronsum=0.0;
-    Real potsum=0.0;
-    Real errorsum_pot=0.0;
     int coord = pf.coordSys();
 
     Real domvol=AMREX_D_TERM((probhi[0]-problo[0]), 
@@ -80,9 +65,7 @@ void main_main()
             const iMultiFab mask = makeFineMask(pf.boxArray(ilev), pf.DistributionMap(ilev),
                                                 pf.boxArray(ilev+1), ratio);
             const MultiFab& mfe = pf.get(ilev, "E");
-            const MultiFab& mfphi = pf.get(ilev, "Potential");
             auto const& ima = mask.const_arrays();
-            auto const& maphi = mfphi.const_arrays();
             auto const& mae = mfe.const_arrays();
             
             electronsum += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
@@ -92,12 +75,6 @@ void main_main()
                                   return((ima[bno](i,j,k) == 0) ? mae[bno](i,j,k)*vol : 0._rt);
                               });
             
-            potsum += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
-                              [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
-                              -> GpuTuple<Real> 
-                              {
-                                  return((ima[bno](i,j,k) == 0) ? maphi[bno](i,j,k)*vol : 0._rt);
-                              });
 
             errorsum_e += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
                               [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
@@ -105,21 +82,12 @@ void main_main()
                               {
                                   IntVect iv(i,j,k);
                                   Real x=xlo+(iv[dir]+0.5)*dxdir;
-                                  Real exactsoln=x*x*x/alpha+n0;
+                                  Real xlen=probhi[dir]-problo[dir];
+                                  Real exactsoln=n0*(1.0+std::sin(PI*x/xlen))*std::exp(-5.0*fintime);
                                   return((ima[bno](i,j,k) == 0) ?  std::pow(mae[bno](i,j,k)-exactsoln,2.0)
                                          *vol : 0._rt);
                               });  
             
-            errorsum_pot += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
-                              [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
-                              -> GpuTuple<Real> 
-                              {
-                                  IntVect iv(i,j,k);
-                                  Real x=xlo+(iv[dir]+0.5)*dxdir;
-                                  Real exactsoln=(std::pow(x,5.0)-x)/40.0;
-                                  return((ima[bno](i,j,k) == 0) ?  std::pow(maphi[bno](i,j,k)-exactsoln,2.0)
-                                         *vol : 0._rt);
-                              });  
         } 
         else 
         {
@@ -135,58 +103,37 @@ void main_main()
                                   return(mae[bno](i,j,k)*vol);
                               });
             
-            potsum += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
-                              [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
-                              -> GpuTuple<Real> 
-                              {
-                                  return(maphi[bno](i,j,k)*vol);
-                              });
-
             errorsum_e += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
                               [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
                               -> GpuTuple<Real> 
                               {
                                   IntVect iv(i,j,k);
                                   Real x=xlo+(iv[dir]+0.5)*dxdir;
-                                  Real exactsoln=x*x*x/alpha+n0;
+                                  Real xlen=probhi[dir]-problo[dir];
+                                  Real exactsoln=n0*(1.0+std::sin(PI*x/xlen))*std::exp(-5.0*fintime);
                                   return(std::pow(mae[bno](i,j,k)-exactsoln,2.0)*vol);
                               });
             
-            errorsum_pot += ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, mfe,
-                              [=] AMREX_GPU_DEVICE (int bno, int i, int j, int k)
-                              -> GpuTuple<Real> 
-                              {
-                                  IntVect iv(i,j,k);
-                                  Real x=xlo+(iv[dir]+0.5)*dxdir;
-                                  Real exactsoln=(std::pow(x,5.0)-x)/40.0;
-                                  return(std::pow(maphi[bno](i,j,k)-exactsoln,2.0)*vol);
-                              });
         }
     }
 
     ParallelDescriptor::ReduceRealSum(errorsum_e);
-    ParallelDescriptor::ReduceRealSum(errorsum_pot);
     ParallelDescriptor::ReduceRealSum(electronsum);
-    ParallelDescriptor::ReduceRealSum(potsum);
     amrex::Real errnorm_e=std::sqrt(errorsum_e/domvol);
-    amrex::Real errnorm_pot=std::sqrt(errorsum_pot/domvol);
     amrex::Real electronavg=electronsum/domvol;
-    amrex::Real potavg=potsum/domvol;
     amrex::Real errfrac_e=errnorm_e/electronavg;
-    amrex::Real errfrac_pot=amrex::Math::abs(errnorm_pot)/amrex::Math::abs(potavg);
 
-    amrex::Print() << "L2 error average of E, pot  = " << errnorm_e << '\t' << errnorm_pot <<"\n";
-    amrex::Print() << "error frac = " << errfrac_e << '\t' << errfrac_pot<<"\n";
-    amrex::Print() << "Electron/pot avg = " << electronavg << '\t' << potavg << "\n";
+    amrex::Print() << "L2 error average of E  = " << errnorm_e << '\n';
+    amrex::Print() << "error frac = " << errfrac_e << '\n';
+    amrex::Print() << "avg Electron = " << electronavg << '\n';
 
-    if(errfrac_e > 1e-3 || errfrac_pot > 1e-3)
+    if(errfrac_e > 1e-3)
     {
-        amrex::Abort("Too much error in MMS2");
+        amrex::Abort("Too much error in MMS3");
     }
     else
     {
-        amrex::Print() << "MMS error less than 1e-3, electron density/potential error="
-        <<errfrac_e<<"\t"<<errfrac_pot<<"\n";
+        amrex::Print() << "MMS error less than 1e-3, error="<<errfrac_e<<"\n";
         amrex::Print()<<"Good to go!!\n";
     }
 
