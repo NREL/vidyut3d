fcec701a745696fdd4f9d5ddadc1fe86396943e7
hsitaram <hariswaran@gmail.com>
2024-10-23 09:14:47 -0600

    turner chemistry fixes

--- test/verification/He_RF_1d/Chemistry.cpp
+++ test/verification/He_RF_1d/Chemistry.cpp
@@ -1,37 +1,4 @@
 #include "Chemistry.H"
-const int rmap[9] = {2, 3, 4, 7, 8, 0, 1, 5, 6};
-
-// Returns 0-based map of reaction order
-void GET_RMAP(int *_rmap) {
-  for (int j = 0; j < 9; ++j) {
-    _rmap[j] = rmap[j];
-  }
-}
-
-// Returns a count of species in a reaction, and their indices
-// and stoichiometric coefficients. (Eq 50)
-void CKINU(const int i, int &nspec, int ki[], int nu[]) {
-  const int ns[9] = {4, 4, 3, 3, 3, 4, 4, 3, 3};
-  const int kiv[36] = {0, 1, 0, 3, 0, 3, 0, 1, 1, 0, 2, 0, 3, 0, 2, 0, 5, 0,
-                       4, 0, 0, 4, 1, 3, 3, 0, 1, 2, 1, 3, 5, 0, 1, 2, 4, 0};
-  const int nuv[36] = {-1, -1, 1, 1, -1, -1, 1, 1, -1, 1,  1, 0,
-                       -1, 1,  1, 0, -1, 1,  1, 0, -1, -1, 1, 1,
-                       -2, 1,  1, 1, -1, -1, 1, 0, -1, -1, 1, 0};
-  if (i < 1) {
-    // Return max num species per reaction
-    nspec = 4;
-  } else {
-    if (i > 9) {
-      nspec = -1;
-    } else {
-      nspec = ns[i - 1];
-      for (int j = 0; j < nspec; ++j) {
-        ki[j] = kiv[(i - 1) * 4 + j] + 1;
-        nu[j] = nuv[(i - 1) * 4 + j];
-      }
-    }
-  }
-}
 
 // save atomic weights into array
 void atomicWeight(amrex::Real *awt) {
@@ -47,7 +14,7 @@ void CKAWT(amrex::Real *awt) { atomicWeight(awt); }
 void CKNCF(int *ncf) {
   int kd = 2;
   // Zero ncf
-  for (int id = 0; id < kd * 6; ++id) {
+  for (int id = 0; id < kd * 5; ++id) {
     ncf[id] = 0;
   }
 
@@ -64,12 +31,8 @@ void CKNCF(int *ncf) {
   // HEm
   ncf[3 * kd + 1] = 1; // He
 
-  // HE2p
-  ncf[4 * kd + 0] = -1; // E
-  ncf[4 * kd + 1] = 2;  // He
-
   // HE2m
-  ncf[5 * kd + 1] = 2; // He
+  ncf[4 * kd + 1] = 1; // He
 }
 
 // Returns the vector of strings of element names
@@ -81,11 +44,10 @@ void CKSYME_STR(amrex::Vector<std::string> &ename) {
 
 // Returns the vector of strings of species names
 void CKSYMS_STR(amrex::Vector<std::string> &kname) {
-  kname.resize(6);
+  kname.resize(5);
   kname[0] = "E";
   kname[1] = "HE";
   kname[2] = "HEp";
   kname[3] = "HEm";
-  kname[4] = "HE2p";
-  kname[5] = "HE2m";
+  kname[4] = "HE2m";
 }
