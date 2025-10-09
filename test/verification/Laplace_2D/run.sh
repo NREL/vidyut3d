56ac092b75d589436cb7ecebd233e645ff5900bd
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-10-09 13:40:09 -0600

    add in neumann condition

--- test/verification/Laplace_2D/run.sh
+++ test/verification/Laplace_2D/run.sh
@@ -2,6 +2,9 @@
 
 export FI_PROVIDER=tcp mpirun
 
+INNER_DIRICHLET=1
+OUTER_DIRICHLET=1
+
 for DIM in 16 32 64 128 256 512
 do
     mkdir -p "${DIM}"
@@ -9,7 +12,7 @@ do
     cd "${DIM}"
     
     rm -rf plt* chk*
-    mpirun -np 8 ../vidyut2d.llvm.MPI.ex ../inputs2d max_step=10 amr.n_cell="${DIM}" "${DIM}" 1
+    mpirun -np 8 ../vidyut2d.llvm.MPI.ex ../inputs2d max_step=10 amr.n_cell="${DIM}" "${DIM}" 1 prob.outer_dirichlet=${OUTER_DIRICHLET} prob.inner_dirichlet=${INNER_DIRICHLET}
     
     cd ..
 done
