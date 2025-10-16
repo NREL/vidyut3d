32531eaa094b7c5854112947c812b981a066c682
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-10-16 10:57:07 -0600

    improve by removing stairstepping

--- test/verification/Laplace_2D/run.sh
+++ test/verification/Laplace_2D/run.sh
@@ -13,6 +13,7 @@ do
     
     rm -rf plt* chk*
     mpirun -np 8 ../vidyut2d.llvm.MPI.ex ../inputs2d max_step=10 amr.n_cell="${DIM}" "${DIM}" 1 prob.outer_dirichlet=${OUTER_DIRICHLET} prob.inner_dirichlet=${INNER_DIRICHLET}
+    ls -1v plt*/Header | tee movie.visit
     
     cd ..
 done
